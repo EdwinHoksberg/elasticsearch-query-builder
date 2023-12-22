4efed9eb375ca1c965ae1c35342994d3c90c6d72
Edwin Hoksberg <mail@edwinhoksberg.nl>
2023-12-22 21:22:12 +0100

    Add Sum Bucket aggregation

---
+++ src/Aggregations/SumBucketAggregation.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EdwinHoksberg\ElasticsearchQueryBuilder\Aggregations;
+
+use EdwinHoksberg\ElasticsearchQueryBuilder\AggregationCollection;
+use EdwinHoksberg\ElasticsearchQueryBuilder\Aggregations\Concerns\WithAggregations;
+
+final class SumBucketAggregation extends Aggregation
+{
+    use WithAggregations;
+
+    protected string $bucketsPath;
+
+    public function __construct(
+        string $name,
+        string $bucketsPath,
+        Aggregation ...$aggregations
+    ) {
+        $this->name = $name;
+        $this->bucketsPath = $bucketsPath;
+        $this->aggregations = new AggregationCollection(...$aggregations);
+    }
+
+    public static function create(
+        string $name,
+        string $bucketsPath,
+        Aggregation ...$aggregations
+    ): self {
+        return new self($name, $bucketsPath, ...$aggregations);
+    }
+
+    /** @return array<string, array<string, string>> */
+    public function payload(): array
+    {
+        return [
+            'sum_bucket' => ['buckets_path' => $this->bucketsPath],
+        ];
+    }
+}
